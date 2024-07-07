9a1a526030840d4da0814dcc018c84269253cb9a
PallaviSingh262215036 <154880018+PallaviSingh262215036@users.noreply.github.com>
2024-07-07 13:14:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,4 @@
-// src/App.js
-/*import React from 'react';
-import MetaMask from './MetaMask';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My Dapp</h1>
-        <MetaMask />
-      </header>
-    </div>
-  );
-}*/
-
-//export default App;
-// App.js
 import React, { useContext, useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import { MetaMaskContext } from './MetaMaskProvider';
